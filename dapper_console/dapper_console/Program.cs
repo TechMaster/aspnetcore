e42cdfaee7406734c9a0d9f052d9e51ba36e2db0
techmaster <cuong@techmaster.vn>
2019-11-01 12:25:20 +0700

    async await

--- dapper_console/dapper_console/Program.cs
+++ dapper_console/dapper_console/Program.cs
@@ -3,6 +3,7 @@ using Dapper;
 using Npgsql;
 using System.Data;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace dapper_console
 {
@@ -16,20 +17,33 @@ namespace dapper_console
     }
     class Program
     {
-        static string _connStr = "Server=192.168.31.155;Port=5432;Database=dvdrental;User Id=user;Password = abc123";
-        static void Main(string[] args)
+        static string _connStr = "Server=localhost;Port=5432;Database=dvdrental;User Id=user;Password=abc123";
+        static async Task Main()
         {
-            PrintData2();
+            dynamic rows = await QueryFilmAsync();
+            foreach (var item in rows)
+            {
+                Console.WriteLine($"{item.film_id}, {item.title}, {item.release_year}");
+            }
+            Console.WriteLine("Done");
         }
 
         public static IDbConnection OpenConnection(string connStr)
         {
             var conn = new NpgsqlConnection(connStr);
-            conn.Open();
+            conn.Open();            
             return conn;
         }
 
-        public static void PrintData()
+        public static async Task<IDbConnection> OpenConnectionAsync(string connStr)
+        {
+            using var conn = new NpgsqlConnection(connStr);
+            await conn.OpenAsync();
+            return conn;
+
+        }
+
+        public static void QueryFilm()
         {
             IList<Film> list;
             using IDbConnection conn = OpenConnection(_connStr);
@@ -49,7 +63,7 @@ namespace dapper_console
             }
         }
 
-        public static void PrintData2()
+        public static void QueryFilmDynamic()
         {
             using IDbConnection conn = OpenConnection(_connStr);
             var querySQL = @"SELECT film_id, title, release_year FROM public.film;";
@@ -58,6 +72,15 @@ namespace dapper_console
             {
                 Console.WriteLine($"{item.film_id}, {item.title}, {item.release_year}");
             }
+
+        }
+
+        //Viết theo phong cách async - await
+        public static async Task<dynamic> QueryFilmAsync()
+        {
+            using var conn = new NpgsqlConnection(_connStr);
+            await conn.OpenAsync();
+            return conn.QueryAsync(@"SELECT film_id, title, release_year FROM public.film").Result;
 
         }
     }
